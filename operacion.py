4ba8525975f6c877f2cb0d6bf5456b0fed3aedfe
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-06-24 19:15:54 -0300

    Se agregó el modulo operacion

---
+++ operacion.py
@@ -0,0 +1,14 @@
+class Operacion:
+    def __init__(self, numero1, numero2):
+        self.num1=numero1
+        self.num2=numero2
+    def sumar(self):
+        return self.num1 + self.num2
+    def restar(self):
+         return self.num1 - self.num2
+    
+class Operacion2:
+    def saludar(self):
+        return "Hola mundo"
+
+    
